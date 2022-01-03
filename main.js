288cb80b9496aacd01d99d23d05411a955228bc2
rachidelaid <rachidelaid@gmail.com>
2022-01-03 12:38:25 +0100

    adding and removing books

---
+++ main.js
@@ -0,0 +1,60 @@
+const listElm = document.querySelector('.books-list');
+const formElm = document.querySelector('.form form');
+
+let booksList = localStorage.getItem('booksList')
+  ? JSON.parse(localStorage.getItem('booksList'))
+  : [];
+
+function addBook(book) {
+  booksList.push(book);
+
+  localStorage.setItem('booksList', JSON.stringify(booksList));
+}
+
+function removeBook(book) {
+  booksList = booksList.filter((currentBook) => {
+    return currentBook.title != book.title;
+  });
+
+  localStorage.setItem('booksList', JSON.stringify(booksList));
+}
+
+function renderBooks() {
+  listElm.innerHTML = '';
+
+  booksList.forEach((book) => {
+    listElm.innerHTML += `
+      <li>
+        <div>
+          <p>${book.title}</p>
+          <p>${book.author}</p>
+          <button>remove</button>
+        </div>
+      </li>
+    `;
+  });
+
+  const removeBtns = document.querySelectorAll('li button');
+  removeBtns.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+      removeBook(booksList[i]);
+
+      btn.parentNode.parentNode.remove();
+    });
+  });
+}
+
+renderBooks();
+
+formElm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = formElm['title'].value.trim();
+  const author = formElm['author'].value.trim();
+
+  formElm['title'].value = '';
+  formElm['author'].value = '';
+
+  addBook({ title, author });
+  renderBooks();
+});
