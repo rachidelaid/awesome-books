969aaa4b811ae5feb35365a614af8941c378e8eb
rachidelaid <rachidelaid@gmail.com>
2022-01-04 14:59:04 +0100

    adding the javascript logic for the navigation

---
+++ navigation.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll('.links a');
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    if (link.classList.contains('active')) return;
+
+    links.forEach((anchor) => anchor.classList.remove('active'));
+
+    link.classList.add('active');
+
+    document.querySelector('.flex').classList.remove('flex');
+
+    document.querySelector(`.${link.id}`).classList.add('flex');
+  });
+});
